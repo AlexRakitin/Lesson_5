8f31e71927b65fe87a8475ff0ee5b3077b86c59a
RAKITEMBO <lewalex18@gmail.com>
2022-10-05 10:34:15 +0300

    1, 3 задачи (в третей не знаю как вывести последнее число)

--- src/Lesson_5.java
+++ src/Lesson_5.java
@@ -2,6 +2,8 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import static java.lang.System.out;
+
 public class Lesson_5 {
     public static void main(String[] args) {
 //        1. Создать трехмерный массив из целых чисел.
@@ -9,27 +11,30 @@ public class Lesson_5 {
 //        элемент на заданное число. Пусть число, на которое будет
 //        увеличиваться каждый элемент, задается из консоли.
 
-//        System.out.println("******ЗАДАЧА_1******");
-//        int[][][] massiveFirst = {
-//                {{1, 2, 3, 4}, {10, 20, 30, 40}},
-//                {{100, 200, 300, 400}}
-//        };
-//
-//
-//        System.out.print("Введите число: ");
-//        Scanner typeOne = new Scanner(System.in);
-//        int typeScannerOne = typeOne.nextInt();
-//
-//        int scannerOne = 0;
-//        for (int[][] massiveFirstOne : massiveFirst) {
-//            for (int[] massiveFirstSecond : massiveFirstOne) {
-//                for (int massiveFirstThird : massiveFirstSecond) {
-//                    scannerOne = massiveFirstThird + typeScannerOne;
-//                    System.out.print(scannerOne + " ");
-//                }
-//                System.out.println();
-//            }
-//        }
+        System.out.println("******ЗАДАЧА_1******");
+        int[][][] massiveFirst = {
+                {{1, 2, 3, 4}, {10, 20, 30, 40}},
+                {{100, 200, 300, 400}}
+        };
+
+
+        System.out.print("Введите число: ");
+        Scanner typeOne = new Scanner(System.in);
+        int typeScannerOne = typeOne.nextInt();
+
+        int scannerOne = 0;
+        for (int[][] massiveFirstOne : massiveFirst) {
+            for (int[] massiveFirstSecond : massiveFirstOne) {
+                for (int massiveFirstThird : massiveFirstSecond) {
+                    scannerOne = massiveFirstThird + typeScannerOne;
+                    System.out.print(scannerOne + " ");
+                }
+                System.out.println();
+            }
+        }
+        out.println("Нажмите любю кнопку для перехода на следующую задачу \n");
+        Scanner enter = new Scanner(System.in);
+        String enterOne = enter.nextLine();
 
 //        3. Создайте двумерный массив целых чисел. Выведите на консоль сумму
 //        всех элементов массива.
